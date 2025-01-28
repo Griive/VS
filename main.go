0c469c30f6b5e0d32d0f2b227f4cf3f1cb742e4e
Ivan <75451187+Griive@users.noreply.github.com>
2025-01-28 23:08:57 +0300

    Update main.go
    fix

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println("ААААА!")
+	fmt.Println("blyyyy")
+	Herny_one()
 }
+
+func Herny_one() {
+	fmt.Println("meny pechataet")
+}
+
 
 // https://www.golang101.com/how-tos/how-to-golang-project-vscode/
